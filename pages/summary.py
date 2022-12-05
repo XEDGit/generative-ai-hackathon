c27fecc65ace816eab3896a60df58dfbbb4438ac
XED <xed@xed.com>
2022-12-05 18:55:21 +0100

    Trying to fix error

--- pages/summary.py
+++ pages/summary.py
@@ -106,8 +106,8 @@ if st.button("Save", disabled=dis_save):
 			fp = open("db/{}{}.png".format(str(i), st.session_state["uuid"]), "w")
 			fp.write(response.content)
 			fp.close()
+		print("trying db", os.path.isdir("db"))
 		if not os.path.isdir("db"):
-			print("created db", os.path.isdir("db"))
 			os.mkdir("db")
 		with open("db/db{}{}.txt".format(str(i), st.session_state["uuid"]), "w") as f:
 			now = datetime.datetime.now()
